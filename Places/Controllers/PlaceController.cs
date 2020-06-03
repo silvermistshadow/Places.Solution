fe8299536d8dadbf9e3e3167837cb1805fbb4c10
silvermistshadow <bmead72@gmail.com>
2020-06-03 09:41:31 -0700

    write more Views, and the cshtml to go with them

--- Places/Controllers/PlaceController.cs
+++ Places/Controllers/PlaceController.cs
@@ -13,5 +13,32 @@ namespace Places.Controllers
             return View(allPlaces);
         }
 
+        [HttpGet("/places/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/places")]
+        public ActionResult Create(string cityName, string stayLength, string journalEntry)
+        {
+            Place newPlace = new Place(cityName, stayLength, journalEntry);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("/places/delete")]
+        public ActionResult DeleteAll()
+        {
+            Place.ClearAll();
+            return View();
+        }
+
+        [HttpGet("/places/{id}")]
+        public ActionResult Show(int id)
+        {
+            Place foundPlace = Place.Find(id);
+            return View(foundPlace);
+        }
+
     }
 }
